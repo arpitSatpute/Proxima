487c2192dd5365a9dbda156638eae6fa018ddc41
arpitSatpute <arpitrameshsatpute6986@gmail.com>
2025-02-22 14:55:45 +0530

    Components Setup

---
+++ components/applicant/ApplicantProfile.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ApplicantProfile = () => {
+  return (
+    <View>
+      <Text>ApplicantProfile</Text>
+    </View>
+  )
+}
+
+export default ApplicantProfile
+
+const styles = StyleSheet.create({})
---
+++ components/mentor/MentorSearch.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MentorSearch = () => {
+  return (
+    <View>
+      <Text>MentorSearch</Text>
+    </View>
+  )
+}
+
+export default MentorSearch
+
+const styles = StyleSheet.create({})
